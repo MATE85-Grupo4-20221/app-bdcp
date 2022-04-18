4ba955b2f19b351a65806d64daa9faef046fc5d6
Igor de Almeida <igoor1205@gmail.com>
2022-04-18 16:53:09 -0300

    feat: improve Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,13 @@ import {
   Button,
   Heading,
   HStack,
+  Image,
   useBreakpointValue,
 } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import logoImage from 'assets/images/computacao-logo.png'
 import { useAuth } from 'contexts/auth'
 
 export interface HeaderProps {}
@@ -19,9 +21,20 @@ export const Header: React.FC<HeaderProps> = () => {
   const isXs = useBreakpointValue({ base: true, sm: false })
 
   return (
-    <HStack as='nav' h='80px' px='8' flexShrink={0} bgColor='primary.500'>
+    <HStack
+      as='nav'
+      position='sticky'
+      top={0}
+      zIndex={10}
+      h='80px'
+      px='8'
+      flexShrink={0}
+      bgColor='primary.500'
+    >
+      <Image w='32px' mr={4} src={logoImage} alt='Estudantes' />
+
       <Heading flex={1} color='white' size='md'>
-        Grupo 4
+        Conteúdos Programáticos
       </Heading>
 
       <Box>
