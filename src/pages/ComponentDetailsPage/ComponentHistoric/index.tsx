4af3452b60d035d3c91dafb13fa0463bb7a32c95
Igor de Almeida <igoor1205@gmail.com>
2022-05-08 14:20:44 -0300

    refactor: change visualization of ComponentHistoric to table

--- src/pages/ComponentDetailsPage/ComponentHistoric/index.tsx
+++ src/pages/ComponentDetailsPage/ComponentHistoric/index.tsx
@@ -1,4 +1,12 @@
-import { Box, Divider, Heading, HStack, Text, VStack } from '@chakra-ui/react'
+import {
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
 import React from 'react'
 
 import { ComponentLog } from 'types'
@@ -9,48 +17,34 @@ export interface ComponentHistoricProps {
 }
 
 const logLabelMap = {
-  approval: 'aprovou alteração desta disciplina.',
-  creation: 'criou esta disciplina.',
-  update: 'alterou o conteúdo desta disciplina.',
+  approval: 'Aprovação',
+  creation: 'Criação',
+  update: 'Atualização',
 }
 
 export const ComponentHistoric: React.FC<ComponentHistoricProps> = ({
   logs,
 }) => {
   return (
-    <VStack pb={4} spacing={0} alignItems='stretch'>
-      {logs.map(log => (
-        <HStack key={log.id} spacing={4} alignItems='flex-start'>
-          <VStack pt={2} spacing={2}>
-            <Box w={6} h={6} bgColor='primary.500' borderRadius={12} />
-
-            <Divider
-              minHeight={94}
-              borderColor='gray.200'
-              orientation='vertical'
-              borderLeftWidth={4}
-            />
-          </VStack>
-
-          <Box
-            px={6}
-            py={4}
-            borderColor='gray.200'
-            borderWidth={3}
-            borderRadius={16}
-          >
-            <Text color='black' fontSize='lg'>
-              <Heading as='span' color='black' fontSize='lg'>
-                {log.user.name}{' '}
-              </Heading>
-              {logLabelMap[log.type]}
-            </Text>
-            <Text color='gray.500' fontSize='sm' fontWeight='semibold'>
-              {formatDate(log.createdAt)}
-            </Text>
-          </Box>
-        </HStack>
-      ))}
-    </VStack>
+    <TableContainer>
+      <Table variant='simple'>
+        <Thead>
+          <Tr>
+            <Th>Nome</Th>
+            <Th>Operação</Th>
+            <Th>Data</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {logs.map(log => (
+            <Tr key={log.id}>
+              <Td>{log.user.name}</Td>
+              <Td>{logLabelMap[log.type]}</Td>
+              <Td>{formatDate(log.createdAt)}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
   )
 }
