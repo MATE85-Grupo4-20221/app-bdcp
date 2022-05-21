b5379f04b93646d2c6051b1ce67b622a0c6fd9d3
Igor de Almeida <igoor1205@gmail.com>
2022-05-21 20:56:56 -0300

    feat: add api requests

---
+++ src/api/component/index.ts
@@ -0,0 +1,50 @@
+import { ComponentLogListFilter } from 'pages'
+import { api } from 'services'
+import { Component, ComponentLog, ListData, ListFilter } from 'types'
+
+export const getComponents = async (
+  filter: ListFilter
+): Promise<ListData<Component>> => {
+  const response = await api.get<ListData<Component>>(`/components`, {
+    params: {
+      page: filter.page,
+      limit: filter.limit,
+      search: filter.search?.trim(),
+    },
+  })
+
+  return {
+    results: response.data.results,
+    total: response.data.total,
+  }
+}
+
+export const getComponentLogs = async (
+  componentId: string,
+  filter: ComponentLogListFilter
+): Promise<ListData<ComponentLog>> => {
+  const response = await api.get<ListData<ComponentLog>>(
+    `/components/${componentId}/logs`,
+    {
+      params: {
+        page: filter.page,
+        limit: filter.limit,
+        type: filter.type || undefined,
+      },
+    }
+  )
+
+  return {
+    results: response.data.results,
+    total: response.data.total,
+  }
+}
+
+export const exportComponent = async (componentId: string): Promise<Blob> => {
+  const response = await api.get<Buffer>(`/components/${componentId}/export`, {
+    responseType: 'arraybuffer',
+    headers: { Accept: 'application/pdf' },
+  })
+
+  return new Blob([response.data], { type: 'application/pdf;charset=utf-8' })
+}
