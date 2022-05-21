923cdffe0ba49c75b3c257ea9d528a19cd50438a
Igor de Almeida <igoor1205@gmail.com>
2022-05-21 20:57:14 -0300

    feat: add redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import 'styles/fonts'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
+import { Provider as ReduxProvider } from 'react-redux'
 
 import { AuthProvider } from 'contexts/auth'
-import { AppRoutes } from './routes'
-import { theme } from './styles'
+import { AppRoutes } from 'routes'
+import { theme } from 'styles'
+import store from 'store'
 
 const App = () => (
-  <ChakraProvider theme={theme}>
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
-  </ChakraProvider>
+  <ReduxProvider store={store}>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </ChakraProvider>
+  </ReduxProvider>
 )
 
 export default App
