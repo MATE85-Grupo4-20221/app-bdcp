8e3d7245b64a2f9c21a12a4989162c389d46543f
Igor de Almeida <igoor1205@gmail.com>
2022-05-21 21:00:15 -0300

    refactor: update ComponentListPage with redux

--- src/pages/ComponentListPage/ComponentListItem/index.tsx
+++ src/pages/ComponentListPage/ComponentListItem/index.tsx
@@ -1,27 +1,17 @@
 import { ListItem } from '@chakra-ui/react'
-import React, { memo, useEffect } from 'react'
-import { NavLink, useParams } from 'react-router-dom'
+import React, { memo } from 'react'
+import { NavLink } from 'react-router-dom'
 
 import { ClassCard } from 'components/ClassCard'
 import { Component } from 'types'
 
 export interface ComponentListItemProps {
   component: Component
-  onSelectComponent: (component: Component) => void
 }
 
 const ComponentList: React.FC<ComponentListItemProps> = ({
   component: { code, name },
-  component,
-  onSelectComponent,
 }) => {
-  const { componentCode } = useParams()
-
-  useEffect(() => {
-    if (code.toLowerCase() === componentCode?.toLowerCase())
-      onSelectComponent(component)
-  }, [code, componentCode])
-
   return (
     <ListItem my={4}>
       <NavLink to={`/disciplinas/${code.toLowerCase()}`}>
