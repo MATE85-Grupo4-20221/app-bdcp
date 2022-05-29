6a7ee6238b5334dd560dba32eda6d6c404fb13fb
Igor de Almeida <igoor1205@gmail.com>
2022-05-29 11:17:13 -0300

    fix: remove ApproveModalForm from ComponentEditPage

--- src/pages/ComponentEditPage/ApproveModalForm/schema.ts
+++
@@ -1,6 +0,0 @@
-import * as Yup from 'yup'
-
-export const approveSchema = Yup.object().shape({
-  agreementDate: Yup.date().required('Campo obrigatório.'),
-  agreementNumber: Yup.string().required('Campo obrigatório.'),
-})
