20932f8dc551c3e75bcff9befeac12424f3c0de3
Iris Nogueira <ires.janine@hotmail.com>
2022-06-11 18:43:52 -0300

    fix: adjust log types on component history table

--- src/types/ComponentLog.ts
+++ src/types/ComponentLog.ts
@@ -2,7 +2,7 @@ import { User } from './User'
 
 export interface ComponentLog {
   id: string
-  type: 'creation' | 'update' | 'approval'
+  type: 'creation' | 'approval' | 'draft_update'
   description?: string
   user: User
   agreementNumber?: string
