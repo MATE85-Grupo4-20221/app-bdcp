524b2f1aa69cb736896f131a646ed53c2d1ce27d
Igor de Almeida <igoor1205@gmail.com>
2022-06-26 18:08:30 -0300

    feat: change color when DataCard is active

--- src/components/DataCard/index.tsx
+++ src/components/DataCard/index.tsx
@@ -4,15 +4,20 @@ import React from 'react'
 export interface DataCardProps {
   label: string
   description?: string
+  isActive?: boolean
 }
 
-export const DataCard: React.FC<DataCardProps> = ({ label, description }) => {
+export const DataCard: React.FC<DataCardProps> = ({
+  label,
+  description,
+  isActive = false,
+}) => {
   return (
     <Box
       px={6}
       py={4}
       flex={1}
-      borderColor='gray.200'
+      borderColor={isActive ? 'primary.100' : 'gray.200'}
       borderWidth={3}
       borderRadius={16}
     >
--- src/pages/ComponentDetailsPage/ComponentOverview/index.tsx
+++ src/pages/ComponentDetailsPage/ComponentOverview/index.tsx
@@ -10,6 +10,7 @@ import React from 'react'
 import { DataCard } from 'components/DataCard'
 
 export interface ComponentOverviewProps {
+  isActive?: boolean
   department?: string
   semester?: string
   modality?: string
@@ -26,6 +27,7 @@ export interface ComponentOverviewProps {
 }
 
 export const ComponentOverview: React.FC<ComponentOverviewProps> = ({
+  isActive = false,
   department,
   semester,
   modality,
@@ -56,22 +58,33 @@ export const ComponentOverview: React.FC<ComponentOverviewProps> = ({
       h='100%'
     >
       <Stack direction={direction} spacing={spacing} alignContent='stretch'>
-        <DataCard label='Departamento' description={department} />
-        <DataCard label='Semestre vigente' description={semester} />
+        <DataCard
+          label='Departamento'
+          description={department}
+          isActive={isActive}
+        />
+        <DataCard
+          label='Semestre vigente'
+          description={semester}
+          isActive={isActive}
+        />
       </Stack>
 
       <Stack direction={direction} spacing={spacing} alignContent='stretch'>
         <DataCard
           label='Carga horária do aluno'
           description={`${studentWorkload} horas`}
+          isActive={isActive}
         />
         <DataCard
           label='Carga horária do professor'
           description={`${teacherWorkload} horas`}
+          isActive={isActive}
         />
         <DataCard
           label='Carga horária do módulo'
           description={`${moduleWorkload} horas`}
+          isActive={isActive}
         />
       </Stack>
 
@@ -79,38 +92,60 @@ export const ComponentOverview: React.FC<ComponentOverviewProps> = ({
         <DataCard
           label='Pré-requisitos'
           description={prerequeriments?.join(' ')}
+          isActive={isActive}
         />
       </HStack>
 
       <HStack spacing={8} alignContent='stretch'>
-        <DataCard label='Modalidade' description={modality} />
+        <DataCard
+          label='Modalidade'
+          description={modality}
+          isActive={isActive}
+        />
       </HStack>
 
       <HStack spacing={8} alignContent='stretch'>
-        <DataCard label='Ementa' description={program} />
+        <DataCard label='Ementa' description={program} isActive={isActive} />
       </HStack>
 
       <HStack spacing={8} alignContent='stretch'>
-        <DataCard label='Objetivo' description={objective} />
+        <DataCard
+          label='Objetivo'
+          description={objective}
+          isActive={isActive}
+        />
       </HStack>
 
       <HStack spacing={8} alignContent='stretch'>
-        <DataCard label='Conteúdo programático' description={syllabus} />
+        <DataCard
+          label='Conteúdo programático'
+          description={syllabus}
+          isActive={isActive}
+        />
       </HStack>
 
       <HStack spacing={8} alignContent='stretch'>
-        <DataCard label='Metodologia' description={methodology} />
+        <DataCard
+          label='Metodologia'
+          description={methodology}
+          isActive={isActive}
+        />
       </HStack>
 
       <HStack spacing={8} alignContent='stretch'>
         <DataCard
           label='Avaliação de aprendizagem'
           description={learningAssessment}
+          isActive={isActive}
         />
       </HStack>
 
       <HStack spacing={8} alignContent='stretch'>
-        <DataCard label='Bibliografia' description={bibliography} />
+        <DataCard
+          label='Bibliografia'
+          description={bibliography}
+          isActive={isActive}
+        />
       </HStack>
     </VStack>
   )
